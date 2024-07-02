f49a306dd5e21b78a13b0f77b86ea420db387f55
itsmealan2000 <itsmealan2000@gmail.com>
2024-07-02 23:23:02 +0530

    back to vercel1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/alanvjohn/"}>
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
