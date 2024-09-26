de4960be34f9bd8013dee9efc9cd88e6c0a4e999
Alan V John <itsmealan2000>
2024-09-26 20:37:11 +0530

    project added

--- src/Pages/project/data/projectdata.jsx
+++ src/Pages/project/data/projectdata.jsx
@@ -3,6 +3,7 @@ import test from './imgs/test.png'
 import interestimg from './imgs/project_interest calculator.png'
 import cardealer from "./imgs/project_CarDealership.png"
 import doctorappoinment from "./imgs/project_doctorappoinment.png"
+import videoconferenceapp from "./imgs/project_videoconferenceapp.png"
 const projectData = [
   {
     id: 1,
@@ -16,14 +17,21 @@ const projectData = [
     title: "Car Dealership",
     description: "A modern car dealership website offering easy browsing, vehicle comparisons, and seamless booking for a hassle-free car buying experience.",
     imgSrc: cardealer,
-    link: "#"
+    link: "https://github.com/itsmealan2000/Car_website.git"
   },
   {
     id: 3,
     title: "Doc Here",
     description: "Effortlessly book doctor appointments, manage health records, and access personalized care through a user-friendly appointment website.",
     imgSrc: doctorappoinment,
-    link: "#"
+    link: "https://github.com/itsmealan2000/DocHere.git"
+  },
+  {
+    id: 4,
+    title: "Video Conference App",
+    description: "Connect seamlessly with a video conferencing app powered by Jitsi, high-quality calls for effortless collaboration.",
+    imgSrc: videoconferenceapp,
+    link: "https://github.com/itsmealan2000/Videoconference_group1.git"
   },
 ];
 
